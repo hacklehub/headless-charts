48b64c40fee528bf536addc289fd2428b47e2fa0
John Pathadan <john.paul@vonnue.com>
2023-07-31 17:04:34 +0530

    fix: story updates

--- src/components/PieChart/PieChart.stories.tsx
+++ src/components/PieChart/PieChart.stories.tsx
@@ -7,15 +7,27 @@ export default {
   tags: ['autodocs'],
 } as Meta;
 
+const classMap = {
+  'Product A': 'text-purple-700',
+  'Product B': 'text-purple-500',
+  'Product C': 'text-purple-300',
+};
+
 export const Default = {
   args: {
     data: [
-      { name: 'Item A', value: 40 },
-      { name: 'Item B', value: 30 },
-      { name: 'Item C', value: 30 },
+      { name: 'Product A', USA: 10000, Europe: 4000, APAC: 5000, Africa: 1000 },
+      { name: 'Product B', USA: 9400, Europe: 9000, APAC: 4000 },
+      { name: 'Product C', USA: 6000, Europe: 4000, APAC: 1000 },
     ],
     id: 'default-pie-chart',
-    value: 'percentage',
-    labels: { key: 'name' },
+    paddingAngle: 45,
+    classNamePoints: { classMap },
+    innerRadius: 0.7,
+    drawing: { duration: 1000 },
+    // // cornerRadius: 90,
+    // value: '100',
+    // labels: { key: 'name' },
+    // paddingBar: 50,
   },
 };
