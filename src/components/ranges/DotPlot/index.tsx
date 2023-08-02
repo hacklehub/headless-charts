7aa353593343fe638fe9a8477b204af9c9bb229d
John Pathadan <john.paul@vonnue.com>
2023-08-02 17:36:52 +0530

    fix: dot plot typescript with updated props

--- src/components/ranges/DotPlot/index.tsx
+++ src/components/ranges/DotPlot/index.tsx
@@ -1,9 +1,8 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import React, { useEffect } from 'react';
-import { ScaleLinear, scaleBand } from 'd3-scale';
 import { axisBottom, axisLeft, axisRight, axisTop } from 'd3-axis';
+import { defaultChartClassNames, mergeTailwindClasses } from '../../../utils';
 import { max, min } from 'd3-array';
 import { pointer, select, selectAll } from 'd3-selection';
+import { scaleBand, scaleLinear } from 'd3-scale';
 import {
   symbol,
   symbolCircle,
@@ -16,8 +15,8 @@ import {
 } from 'd3-shape';
 
 import { ChartProps } from '../../../types';
-import { defaultChartClassNames } from '../../../utils';
-import { mergeTailwindClasses } from '../../../utils';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useEffect } from 'react';
 import { zoom } from 'd3-zoom';
 
 interface DotPlotProps extends ChartProps {
@@ -26,242 +25,298 @@ interface DotPlotProps extends ChartProps {
   y: {
     [key: string]: string;
   };
-  x: {any: any};
+  x: {
+    start: number;
+    end: number;
+    minKey: any;
+    maxKey: any;
+    axis: 'bottom' | 'top';
+    axisTicks: number;
+  };
   tooltip?: {
     className?: string;
+    kays?: string[];
     html?: (d: any) => string;
   };
 }
 
 const DotPlot = ({
-    id,
-    className,
-    data = [],
-    classNameData,
-    y = { key: "label" },
-    x = {},
-    marginTop = 40,
-    marginBottom = 40,
-    marginLeft = 40,
-    marginRight = 20,
-    paddingTop = 0,
-    paddingLeft = 0,
-    paddingRight = 0,
-    paddingBottom = 0,
-    shape = "circle",
-    tooltip = {},
-    zooming,
+  id,
+  className,
+  data = [],
+  classNameData,
+  y = { key: 'label' },
+  x,
+  margin = {
+    top: 40,
+    bottom: 40,
+    left: 40,
+    right: 20,
+  },
+  padding = {
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+  },
+  shape = 'circle',
+  tooltip = {},
+  zooming,
 }: DotPlotProps) => {
-    const refreshChart = async () => {
-        const shapeMapping = {
-          circle: symbolCircle,
-          diamond: symbolDiamond,
-          triangle: symbolTriangle,
-          square: symbolSquare,
-          cross: symbolCross,
-          star: symbolStar,
-          wye: symbolWye,
-        };
-        const svg = select(`#${id}`);
-        // Clear svg
-    
-        svg.selectAll("*").remove();
-    
-        const width = +svg.style("width").split("px")[0],
-          height = +svg.style("height").split("px")[0];
-    
-        const g = svg.append("g");
-    
-        const xFn = scaleLinear()
-          .domain([
-            Number.isFinite(x.start) ? x.start : min(data.map(d => d[x.minKey])),
-            Number.isFinite(x.end) ? x.end : max(data.map(d => d[x.maxKey])),
-          ])
-          .range([marginLeft, width - paddingRight - marginRight]);
-    
-        const yFn = scaleBand()
-          .domain(data.map(d => d[y.key]))
-          .range([marginTop + paddingTop, height - marginBottom - paddingBottom]);
-    
-        svg
-          .append("clipPath")
-          .attr("id", "clip")
-          .append("rect")
-          .attr("x", marginLeft)
-          .attr("y", marginTop - paddingTop - 10)
-          .attr("width", width)
-          .attr("height", height + paddingBottom + 8);
-    
-        const dataG = g
-          .append("g")
-          .attr("class", "data")
-          .attr("clip-path", "url(#clip)");
-    
-        const dotRowsG = dataG
-          .selectAll("g")
-          .data(data)
-          .enter()
-          .append("g")
-          .on("mouseenter", function (event, d) {
-            tooltip && tooltipDiv.style("opacity", 1);
-            const [bX, bY] = pointer(event, select("body"));
-            tooltipDiv.style("left", `${bX + 10}px`).style("top", `${bY + 10}px`);
-            tooltipDiv.html(
-              tooltip && tooltip.html
-                ? tooltip.html(d)
-                : tooltip.keys
-                ? tooltip.keys.map(key => `${key}: ${d[key] || ""}`).join("<br/>")
-                : `${d[y.key]}: ${d[x.minKey]} to ${d[x.maxKey]}`,
-            );
-          })
-          .on("mouseleave", function () {
-            tooltip &&
-              tooltipDiv
-                .style("opacity", "0")
-                .style("left", `0px`)
-                .style("top", `0px`);
-          });
-    
-        dotRowsG
-          .append("line")
-          .attr("clip-path", "url(#clip)")
-          .attr("x1", d => xFn(d[x.minKey]))
-          .attr("x2", d => xFn(d[x.minKey]))
-          .attr("y1", d => yFn(d[y.key]) + yFn.bandwidth() / 2)
-          .attr("y2", d => yFn(d[y.key]) + yFn.bandwidth() / 2)
-          .attr("class", `dot-plot-line stroke-current ${classNameData || ``}`)
-          .transition()
-          .duration(1000)
-          .attr("x2", d => xFn(d[x.maxKey]));
-    
-        dotRowsG
-          .append("path")
-          .attr("class", `start-dots fill-current ${classNameData || ""} `)
-          .attr("d", d => symbol(shapeMapping[shape], 100)())
-          .attr(
-            "transform",
-            d =>
-              `translate(${xFn(d[x.minKey])},${
-                yFn(d[y.key]) + yFn.bandwidth() / 2
-              })`,
-          );
-    
-        dotRowsG
-          .append("path")
-          .attr("class", `end-dots fill-current ${classNameData || ""} `)
-          .attr("d", d => symbol(shapeMapping[shape], 100)())
-          .attr(
-            "transform",
-            d =>
-              `translate(${xFn(d[x.minKey])},${
-                yFn(d[y.key]) + yFn.bandwidth() / 2
-              })`,
+  const refreshChart = async () => {
+    const shapeMapping = {
+      circle: symbolCircle,
+      diamond: symbolDiamond,
+      triangle: symbolTriangle,
+      square: symbolSquare,
+      cross: symbolCross,
+      star: symbolStar,
+      wye: symbolWye,
+    };
+    const svg = select(`#${id}`);
+    // Clear svg
+
+    svg.selectAll('*').remove();
+
+    const width = +svg.style('width').split('px')[0],
+      height = +svg.style('height').split('px')[0];
+
+    const g = svg.append('g');
+
+    const xFn = scaleLinear()
+      /* eslint-disable */
+      .domain([
+        // @ts-ignore
+        Number.isFinite(x.start) ? x.start : min(data.map((d) => d[x.minKey])),
+        // @ts-ignore
+        Number.isFinite(x.end) ? x.end : max(data.map((d) => d[x.maxKey])),
+      ])
+      // @ts-ignore
+      .range([margin.left, width - padding.right - margin.right]);
+
+    const yFn = scaleBand()
+      // @ts-ignore
+      .domain(data.map((d) => d[y.key]))
+      .range([
+        // @ts-ignore
+        margin.top + padding.top,
+        // @ts-ignore
+        height - margin.bottom - padding.bottom,
+      ]);
+
+    svg
+      .append('clipPath')
+      .attr('id', 'clip')
+      .append('rect')
+      .attr('x', margin.left)
+      // @ts-ignore
+      .attr('y', margin.top - padding.top - 10)
+      .attr('width', width)
+      // @ts-ignore
+      .attr('height', height + padding.bottom + 8);
+
+    const dataG = g
+      .append('g')
+      .attr('class', 'data')
+      .attr('clip-path', 'url(#clip)');
+
+    const dotRowsG = dataG
+      .selectAll('g')
+      .data(data)
+      .enter()
+      .append('g')
+      .on('mouseenter', function (event, d) {
+        tooltip && tooltipDiv.style('opacity', 1);
+        const [bX, bY] = pointer(event, select('body'));
+        tooltipDiv.style('left', `${bX + 10}px`).style('top', `${bY + 10}px`);
+        tooltipDiv.html(
+          tooltip && tooltip.html
+            ? tooltip.html(d)
+            : // @ts-ignore
+            tooltip.keys
+            ? // @ts-ignore
+              tooltip.keys.map((key) => `${key}: ${d[key] || ''}`).join('<br/>')
+            : // @ts-ignore
+              `${d[y.key]}: ${d[x.minKey]} to ${d[x.maxKey]}`
+        );
+      })
+      .on('mouseleave', function () {
+        tooltip &&
+          tooltipDiv
+            .style('opacity', '0')
+            .style('left', `0px`)
+            .style('top', `0px`);
+      });
+
+    dotRowsG
+      .append('line')
+      .attr('clip-path', 'url(#clip)')
+      // @ts-ignore
+      .attr('x1', (d) => xFn(d[x.minKey]))
+      // @ts-ignore
+      .attr('x2', (d) => xFn(d[x.minKey]))
+      // @ts-ignore
+      .attr('y1', (d) => yFn(d[y.key]) + yFn.bandwidth() / 2)
+      // @ts-ignore
+      .attr('y2', (d) => yFn(d[y.key]) + yFn.bandwidth() / 2)
+      .attr('class', `dot-plot-line stroke-current ${classNameData || ``}`)
+      .transition()
+      .duration(1000)
+      // @ts-ignore
+      .attr('x2', (d) => xFn(d[x.maxKey]));
+
+    dotRowsG
+      .append('path')
+      .attr('class', `start-dots fill-current ${classNameData || ''} `)
+      .attr('d', () => symbol(shapeMapping[shape], 100)())
+      .attr(
+        'transform',
+        (d) =>
+          // @ts-ignore
+          `translate(${xFn(d[x.minKey])},${
+            // @ts-ignore
+            yFn(d[y.key]) + yFn.bandwidth() / 2
+          })`
+      );
+
+    dotRowsG
+      .append('path')
+      .attr('class', `end-dots fill-current ${classNameData || ''} `)
+      .attr('d', () => symbol(shapeMapping[shape], 100)())
+      .attr(
+        'transform',
+        (d) =>
+          // @ts-ignore
+          `translate(${xFn(d[x.minKey])},${
+            // @ts-ignore
+            yFn(d[y.key]) + yFn.bandwidth() / 2
+          })`
+      )
+      .transition()
+      .duration(1000)
+      .attr(
+        'transform',
+        (d) =>
+          // @ts-ignore
+          `translate(${xFn(d[x.maxKey])},${
+            // @ts-ignore
+            yFn(d[y.key]) + yFn.bandwidth() / 2
+          })`
+      );
+
+    const redraw = () => {
+      selectAll('.dot-plot-line')
+        // @ts-ignore
+        .attr('x1', (d) => xFn(d[x.minKey]))
+        // @ts-ignore
+        .attr('x2', (d) => xFn(d[x.maxKey]));
+
+      selectAll('.start-dots').attr(
+        'transform',
+        (d) =>
+          // @ts-ignore
+          `translate(${xFn(d[x.minKey])},${
+            // @ts-ignore
+            yFn(d[y.key]) + yFn.bandwidth() / 2
+          })`
+      );
+      selectAll('.end-dots').attr(
+        'transform',
+        (d) =>
+          // @ts-ignore
+          `translate(${xFn(d[x.maxKey])},${
+            // @ts-ignore
+            yFn(d[y.key]) + yFn.bandwidth() / 2
+          })`
+      );
+    };
+
+    const yAxis = y.axis === 'right' ? axisRight(yFn) : axisLeft(yFn);
+
+    const yAxisG = g
+      .append('g')
+      .attr('class', 'yAxis axis')
+      .attr(
+        'transform',
+        `translate(${y.axis === 'right' ? margin.left + width : margin.left},0)`
+      );
+
+    yAxisG.call(yAxis);
+
+    padding.bottom &&
+      yAxisG
+        .append('line')
+        .attr('x1', 0)
+        .attr('x2', 0)
+        .attr('y1', margin.top + height - padding.bottom)
+        .attr('y2', margin.top + height)
+        .attr('stroke', 'currentColor');
+
+    const xAxis =
+      x.axis === 'top'
+        ? axisTop(xFn).ticks(x.axisTicks || 5)
+        : axisBottom(xFn).ticks(x.axisTicks || 5);
+
+    const xAxisG = g.append('g').attr('class', 'axis--x axis ');
+
+    xAxisG
+      .attr(
+        'transform',
+        `translate(0, ${
+          x.axis === 'top' ? margin.top : height - margin.bottom
+        })`
+      )
+      .call(xAxis);
+
+    const tooltipDiv = select('body')
+      .append('div')
+      .attr('id', 'tooltip')
+      .style('position', 'absolute')
+      .style('opacity', '0')
+      .attr('class', `tooltip ${(tooltip && tooltip.className) || ''}`);
+
+    if (zooming) {
+      const extent = [
+        [margin.left, margin.top],
+        [width, height],
+      ];
+
+      const zoomFunc = zoom()
+        .scaleExtent([1, 4])
+        // @ts-ignore
+        .extent(extent)
+        // @ts-ignore
+        .translateExtent(extent)
+        .on('zoom', zoomed);
+
+      // @ts-ignore
+      function zoomed(event) {
+        xFn.range(
+          // @ts-ignore
+          [margin.left, width - padding.right - margin.right].map((d) =>
+            event.transform.applyX(d)
           )
-          .transition()
-          .duration(1000)
-          .attr(
-            "transform",
-            d =>
-              `translate(${xFn(d[x.maxKey])},${
-                yFn(d[y.key]) + yFn.bandwidth() / 2
-              })`,
-          );
-    
-        const redraw = () => {
-          selectAll(".dot-plot-line")
-            .attr("x1", d => xFn(d[x.minKey]))
-            .attr("x2", d => xFn(d[x.maxKey]));
-    
-          selectAll(".start-dots").attr(
-            "transform",
-            d =>
-              `translate(${xFn(d[x.minKey])},${
-                yFn(d[y.key]) + yFn.bandwidth() / 2
-              })`,
-          );
-          selectAll(".end-dots").attr(
-            "transform",
-            d =>
-              `translate(${xFn(d[x.maxKey])},${
-                yFn(d[y.key]) + yFn.bandwidth() / 2
-              })`,
-          );
-        };
-    
-        const yAxis = y.axis === "right" ? axisRight(yFn) : axisLeft(yFn);
-    
-        const yAxisG = g
-          .append("g")
-          .attr("class", "yAxis axis")
-          .attr(
-            "transform",
-            `translate(${y.axis === "right" ? marginLeft + width : marginLeft},0)`,
-          );
-    
-        yAxisG.call(yAxis);
-    
-        paddingBottom &&
-          yAxisG
-            .append("line")
-            .attr("x1", 0)
-            .attr("x2", 0)
-            .attr("y1", marginTop + height - paddingBottom)
-            .attr("y2", marginTop + height)
-            .attr("stroke", "currentColor");
-    
-        const xAxis =
-          x.axis === "top"
-            ? axisTop(xFn).ticks(x.axisTicks || 5)
-            : axisBottom(xFn).ticks(x.axisTicks || 5);
-    
-        const xAxisG = g.append("g").attr("class", "axis--x axis ");
-    
-        xAxisG
-          .attr(
-            "transform",
-            `translate(0, ${x.axis === "top" ? marginTop : height - marginBottom})`,
-          )
-          .call(xAxis);
-    
-        const tooltipDiv = select("body")
-          .append("div")
-          .attr("id", "tooltip")
-          .style("position", "absolute")
-          .style("opacity", "0")
-          .attr("class", `tooltip ${(tooltip && tooltip.className) || ""}`);
-    
-        if (zooming) {
-          const extent = [
-            [marginLeft, marginTop],
-            [width, height],
-          ];
-    
-          const zoomFunc = zoom()
-            .scaleExtent([1, 4])
-            .extent(extent)
-            .translateExtent(extent)
-            .on("zoom", zoomed);
-    
-          function zoomed(event) {
-            xFn.range(
-              [marginLeft, width - paddingRight - marginRight].map(d =>
-                event.transform.applyX(d),
-              ),
-            );
-            xAxisG.call(xAxis);
-            redraw();
-          }
-          svg.call(zoomFunc);
-        }
-      };
-    
-      useEffect(() => {
-        refreshChart();
-        return () => {
-          selectAll(".tooltip").remove();
-        };
-      }, [data]);
-  return <div></div>;
+        );
+        xAxisG.call(xAxis);
+        redraw();
+      }
+      /* eslint-enable */
+      svg.call(zoomFunc);
+    }
+  };
+
+  useEffect(() => {
+    refreshChart();
+    return () => {
+      selectAll('.tooltip').remove();
+    };
+  }, [data]);
+  return (
+    <svg
+      id={id}
+      className={mergeTailwindClasses(defaultChartClassNames, className)}
+    />
+  );
 };
 
 export default DotPlot;
