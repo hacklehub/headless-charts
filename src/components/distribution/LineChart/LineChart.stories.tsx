d258fa43cd1bab92c434c65340e41d25350e90e4
John Pathadan <john.paul@vonnue.com>
2023-08-07 15:14:37 +0530

    feat: adding stories to storybook continuing

--- src/components/distribution/LineChart/LineChart.stories.tsx
+++ src/components/distribution/LineChart/LineChart.stories.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import './index.css';
 
 import LineChart from '.';
@@ -132,11 +133,233 @@ export const Zooming = {
 
 export const WithTooltip = {
   args: {
-    ...WithStyle.args,
+    data,
     id: 'line-chart-with-tooltip',
+    x: { key: 'id' },
+    y: [
+      { key: 'value', className: 'text-green-500' },
+      { key: 'reading', className: 'text-blue-500' },
+    ],
     tooltip: {
-      className: 'bg-gray-800 text-white p-2 rounded',
-      keys: ['value', 'reading'],
+      keys: ['value', 'reading', 'id'],
+      className: 'text-green-500',
+      html: (row: any) => `${row.id} - ${row.value || ''}/${row.reading || ''}`,
     },
+    showGuideLines: true,
   },
 };
+
+export const XAxisLabel = {
+  args: {
+    data,
+    id: 'line-chart-with-x-axis-label',
+    x: { key: 'id', axisLabel: 'Some Index' },
+    y: [
+      { key: 'value', className: 'text-green-500' },
+      { key: 'reading', className: 'text-blue-500' },
+    ],
+    className: '',
+  },
+};
+
+export const WithXAxisCustomStart = {
+  args: {
+    data,
+    id: 'line-chart-with-x-axis-custom-start',
+    x: { key: 'id', axisLabel: 'Some Index', start: 0, end: 7 },
+    y: [
+      { key: 'value', className: 'text-green-500' },
+      { key: 'reading', axis: 'right', className: 'text-blue-500' },
+    ],
+    className: '',
+  },
+};
+export const WithXAxisAtTop = {
+  args: {
+    data,
+    id: 'x-axis-at-top',
+    x: { key: 'id', axisLabel: 'Some Index', axis: 'top' },
+    y: [
+      { key: 'value', className: 'text-green-500' },
+      {
+        key: 'reading',
+        axis: 'right',
+        className: 'text-blue-500',
+      },
+    ],
+    className: '',
+  },
+};
+
+export const WithXAxisTicks = {
+  args: {
+    data,
+    id: 'x-axis-ticks',
+    x: { key: 'id', axisLabel: 'Some Index', axisTicks: 15 },
+    y: [
+      { key: 'value', className: 'text-green-500' },
+      { key: 'reading', className: 'text-blue-500' },
+    ],
+    className: '',
+  },
+};
+
+export const YAxisCustomStart = {
+  args: {
+    data,
+    id: 'y-axis-custom-start',
+    x: { key: 'id', axisLabel: 'Some Index' },
+    y: [
+      { key: 'value', className: 'text-green-500', start: 0 },
+      {
+        key: 'reading',
+        className: 'text-blue-500',
+        axis: 'right',
+        start: 0,
+        ticks: 3,
+      },
+    ],
+    className: '',
+  },
+};
+
+export const YAxisCustomSymbolChart = {
+  args: {
+    data,
+    id: 'y-axis-custom-label-chart',
+    x: { key: 'id', axisLabel: 'Some Index' },
+    y: [
+      {
+        key: 'value',
+        className: 'text-green-500',
+        start: 0,
+        symbol: 'diamond',
+      },
+      {
+        key: 'reading',
+        symbol: 'circle',
+        className: 'text-blue-500',
+        axis: 'right',
+        start: 0,
+        ticks: 3,
+      },
+    ],
+    className: '',
+  },
+};
+
+export const YAxisUnknown = {
+  args: {
+    data,
+    id: 'y-axis-unknown-chart',
+    x: { key: 'id', axisLabel: 'Some Index' },
+    y: [
+      {
+        key: 'value',
+        className: 'text-green-500',
+        start: 0,
+        symbol: 'diamond',
+        unknown: 'zero',
+      },
+      {
+        key: 'reading',
+        symbol: 'circle',
+        className: 'text-blue-500',
+        axis: 'right',
+        unknown: 'zero',
+        start: 0,
+        ticks: 3,
+      },
+    ],
+    className: '',
+  },
+};
+
+export const YAxisLabel = {
+  args: {
+    data,
+    id: 'y-axis-custom-label-chart',
+    x: { key: 'id', axisLabel: 'Some Index' },
+    y: [
+      {
+        key: 'value',
+        className: 'text-green-500',
+        start: 0,
+        symbol: 'diamond',
+        axisLabel: 'Volume',
+      },
+      {
+        key: 'value',
+        symbol: 'circle',
+        axisLabel: 'Pressure',
+        className: 'text-blue-500',
+        axis: 'right',
+        start: 0,
+        ticks: 3,
+      },
+    ],
+    className: '',
+  },
+};
+
+export const YAxisCurve = {
+  args: {
+    data,
+    id: 'y-axis-custom-curve-chart',
+    x: { key: 'id', axisLabel: 'Some Index' },
+    y: [
+      {
+        key: 'value',
+        className: 'text-green-500',
+        start: 0,
+        symbol: 'diamond',
+        curve: 'step',
+      },
+      {
+        key: 'reading',
+        symbol: 'circle',
+        className: 'text-blue-500',
+        axis: 'right',
+        start: 0,
+        ticks: 3,
+      },
+    ],
+    className: '',
+  },
+};
+
+export const LineChartVertical = {
+  args: {
+    data,
+    id: 'horizontal-line-chart',
+    x: { key: 'id' },
+    y: [
+      { key: 'value', className: 'text-green-500' },
+      { key: 'reading', className: 'text-blue-500' },
+    ],
+    tooltip: { keys: ['id', 'value', 'reading'] },
+    referenceLines: [
+      { x: 4, className: 'stroke-current text-blue-200 stroke-2 dashed' },
+    ],
+  },
+};
+
+export const LineChartHorizontal = {
+  args: {
+    data,
+    id: 'vertical-line-chart',
+    x: { key: 'id' },
+    y: [
+      { key: 'value', className: 'text-green-500' },
+      { key: 'reading', className: 'text-blue-500' },
+    ],
+    tooltip: { keys: ['id', 'value', 'reading'] },
+    referenceLines: [
+      {
+        yLeft: 1600,
+        className: 'stroke-current text-blue-200, stroke-1 dashed',
+      },
+    ],
+  },
+};
+
