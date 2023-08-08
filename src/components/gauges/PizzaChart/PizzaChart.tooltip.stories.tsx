6fbcc71b6091aea402b2ac51871c95b91412a79d
jayaramkasi <jayaramkasi@gmail.com>
2023-08-08 22:30:31 +0530

    Tooltip stories

--- src/components/gauges/PizzaChart/PizzaChart.tooltip.stories.tsx
+++ src/components/gauges/PizzaChart/PizzaChart.tooltip.stories.tsx
@@ -1,7 +1,33 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Meta, StoryObj } from '@storybook/react';
 
 import PizzaChart from '.';
 import data from './sample.json';
+
+const metrics = [
+  {
+    key: 'metric1',
+  },
+  {
+    key: 'metric2',
+  },
+  {
+    key: 'metric3',
+  },
+  {
+    key: 'metric4',
+  },
+  {
+    key: 'metric5',
+  },
+  {
+    key: 'metric6',
+  },
+  {
+    key: 'metric7',
+  },
+];
 
 export default {
   title: 'Gauge/PizzaChart/Tooltip',
@@ -15,28 +41,29 @@ export const WithTooltip: Story = {
     id: 'pizza-chart-tooltip',
     data,
     tooltip: {},
-    metrics: [
-      {
-        key: 'metric1',
-      },
-      {
-        key: 'metric2',
-      },
-      {
-        key: 'metric3',
-      },
-      {
-        key: 'metric4',
-      },
-      {
-        key: 'metric5',
-      },
-      {
-        key: 'metric6',
-      },
-      {
-        key: 'metric7',
-      },
-    ],
+    metrics,
+  },
+};
+
+export const WithCustomColors: Story = {
+  args: {
+    ...WithTooltip.args,
+    id: 'pizza-chart-tooltip-custom-colors',
+    tooltip: {
+      className: 'bg-white border-2 border-purple-700 p-2 rounded',
+    },
+  },
+};
+
+export const WithCustomHtml: Story = {
+  args: {
+    ...WithTooltip.args,
+    id: 'pizza-chart-tooltip-custom-html',
+    tooltip: {
+      html: (d: { index: number; data: number }) =>
+        `<div class="bg-white border-2 border-purple-700 p-2 rounded">
+        ${metrics[d.index].key} was scored at ${data[metrics[d.index].key]}.
+        </div>`,
+    },
   },
 };
