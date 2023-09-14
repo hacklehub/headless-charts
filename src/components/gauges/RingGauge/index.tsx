d39345d37235ca33779662c8f1f7300d307fac8e
John Pathadan <john.paul@vonnue.com>
2023-09-14 20:15:26 +0530

    refactor: cleaned

--- src/components/gauges/RingGauge/index.tsx
+++ src/components/gauges/RingGauge/index.tsx
@@ -1,6 +1,6 @@
 import { BaseType, Selection } from 'd3';
 import { PieArcDatum, arc } from 'd3-shape';
-import React, { useEffect, useRef } from 'react';
+import React, { useCallback, useEffect, useRef } from 'react';
 import { defaultChartClassNames, mergeTailwindClasses } from '../../../utils';
 import { select, selectAll } from 'd3-selection';
 
@@ -15,10 +15,10 @@ type ToolTipType = {
 };
 
 type MarginProps = {
-  top?: number | undefined;
-  bottom?: number | undefined;
-  left?: number | undefined;
-  right?: number | undefined;
+  top?: number;
+  bottom?: number;
+  left?: number;
+  right?: number;
 };
 
 type LabelProps = {
@@ -39,7 +39,7 @@ export interface RingGaugeProps {
   dataKey: string;
   errorKey?: string;
   labels?: LabelProps;
-  data?: unknown[];
+  data?: Array<Record<string, number | string>>;
   margin?: MarginProps;
   padding?: {
     arc?: number;
@@ -54,10 +54,9 @@ export interface RingGaugeProps {
   classNameGaugeBg?: string;
 }
 
-const RingGauge = ({
+const RingGauge: React.FC<RingGaugeProps> = ({
   className,
   id,
-  labelKey,
   targetKey,
   dataKey,
   data = [],
@@ -75,46 +74,29 @@ const RingGauge = ({
   drawing = { duration: 1000, delay: 0 },
   startAngle = 0,
   endAngle = 270,
-  tooltip,
   classNameGaugeBg = '',
-  labels = { position: 'top' },
 }: RingGaugeProps) => {
+  const PI = Math.PI;
+  const numArcs = data.length;
+  const previousData = useRef<Array<Record<string, string | number>>>([]);
 
-
-
-    /************UTILITIES*********** */
-
-  function addTooltips(tooltip:ToolTipType) {
-    const tooltipDiv = tooltip && select('#tooltip').node()
-      ? select('#tooltip')
-      : select('body')
-        .append('div')
-        .attr('id', 'tooltip')
-        .style('position', 'absolute')
-        .style('opacity', '0')
-        .attr('class', mergeTailwindClasses(tooltip?.className));
-
-    return { tooltipDiv };
-  }
-  const PI = Math.PI
-  const numArcs = data.length; 
-  const previousData = useRef<any[]>([]);
-
-  const refreshChart = React.useCallback(() => {
-
+  const refreshChart = useCallback(() => {
     transition();
-    const getInnerRadius = (index: number, arcWidth: number ) => minRadius + (numArcs - (index + 1)) * (arcWidth + (padding.arc || 0))
-    const getOuterRadius = (index: number, arcWidth:number) => getInnerRadius(index,arcWidth) + arcWidth;
-    
-    // Find the SVG
-    const svg = select(`#${id}`);
-    // Remove all the elements
-    cleanSVG(svg);
-    const { width, height } = getSVGDimensions(svg)
+
+    const getInnerRadius = (index: number, arcWidth: number) =>
+      minRadius + (numArcs - (index + 1)) * (arcWidth + (padding.arc || 0));
+
+    const getOuterRadius = (index: number, arcWidth: number) =>
+      getInnerRadius(index, arcWidth) + arcWidth;
+
+    const svg = select<SVGElement, unknown>(`#${id}`);
+    svg.selectAll('*').remove();
+
+    const { width, height } = getSVGDimensions(svg);
     const g = svg.append('g');
     g.attr('transform', `translate(${width / 2},${height / 2})`);
 
-    const {chartRadius} = getRadialDimensions(width, margin, height);
+    const { chartRadius } = getRadialDimensions(width, margin, height);
 
     const scale = scaleLinear()
       .domain([0, 1])
@@ -123,11 +105,11 @@ const RingGauge = ({
     const arcWidth =
       (chartRadius - minRadius - numArcs * (padding.arc || 5)) / numArcs;
 
-    const arcFn = arc<PieArcDatum<any> | number>()
-      .innerRadius((_d: any, i: number) => getInnerRadius(i, arcWidth))
-      .outerRadius((_d: any, i: number) => getOuterRadius(i, arcWidth))
+    const arcFn = arc<PieArcDatum<unknown> | number>()
+      .innerRadius((_d, i) => getInnerRadius(i, arcWidth))
+      .outerRadius((_d, i) => getOuterRadius(i, arcWidth))
       .startAngle(((startAngle / 90) * PI) / 2)
-      .endAngle((d: any) => scale(d))
+      .endAngle((d) => scale(d))
       .cornerRadius(cornerRadius);
 
     g.append('g')
@@ -143,11 +125,10 @@ const RingGauge = ({
           classNameGaugeBg
         )
       )
-      .attr('d', function (_d, i: number) {
-        return arcFn(1, i);
+      .attr('d', function (_d, i) {
+        return arcFn(1, i) as string;
       });
 
-    const {  tooltipDiv } = tooltip? addTooltips(tooltip) : {tooltipDiv:<div></div>};
     g.append('g')
       .attr('class', 'data')
       .selectAll('path')
@@ -155,97 +136,58 @@ const RingGauge = ({
       .enter()
       .append('path')
       .attr('class', (d) =>
-        mergeTailwindClasses('data-arc fill-current ', d.className)
+        mergeTailwindClasses('data-arc fill-current ', d.className?.toString())
       )
       .attr('d', '')
-      .on('mouseenter', function (_event, d) {
-        tooltipDiv.attr(
-          'class',
-          mergeTailwindClasses(tooltip?.className, 'tooltip')
-        );
-        tooltipDiv
-          .style('opacity', 1)
-          .html(
-            tooltip?.html
-              ? tooltip.html(d)
-              : `${d[labelKey]} <br/>${d[dataKey]}/${d[targetKey]}`
-          );
-      })
-    
-      .on('mousemove', function (event) {
-        const [bX, bY] = pointer(event, select('body'));
-        tooltipDiv.style('left', `${bX + 10}px`).style('top', `${bY + 10}px`);
-      })
-      .on('mouseleave', function () {
-        tooltipDiv.style('opacity', 0);
-        tooltipDiv.html('');
-      })
       .transition()
       .duration(drawing?.duration || 1000)
       .delay(drawing?.delay || 0)
-      .attrTween('d', (d: any, i: number) => {
+      .attrTween('d', (d, i) => {
         const previousArc = previousData?.current?.find(
           (a) => a['name'] === d['name']
         );
 
+        const interpolateArg1: number = previousArc ? +previousArc[dataKey] : 0;
+        const factor = +d[dataKey] / +d[targetKey] || 0;
+        const interpolateArg2: number | undefined = min([factor, 1]) || 0;
         const interpolate = interpolateNumber(
-          previousArc?.[dataKey] || 0,
-          min([d[dataKey] / d[targetKey], 1])
+          interpolateArg1,
+          interpolateArg2
         );
-        return (t: number) => arcFn(interpolate(t), i);
+        return (t: number) => arcFn(interpolate(t), i) as string;
       });
 
     const timeOut = setTimeout(() => {
       previousData.current = data;
-    }, drawing?.duration);
-
-    labels &&
-      g
-        .append('g')
-        .attr('class', 'labels')
-        .selectAll('.labels')
-        .data(data)
-        .enter()
-        .append('text')
-        .attr('text-anchor', 'end')
-        .attr('class', `fill-current text-xs ${labels.className || ''}`)
-        .attr('x', -5)
-        .attr('y', (_d, i) =>
-          labels?.position === 'bottom'
-            ? getInnerRadius(i,arcWidth) + arcWidth - 1
-            : -getInnerRadius(i, arcWidth) - 2
-        )
-        .text((d: unknown) => d[labelKey]);
+    }, drawing?.duration || 0);
 
     return () => {
       clearTimeout(timeOut);
     };
   }, [
-    id, 
-    margin, 
-    endAngle, 
-    PI, 
-    minRadius, 
-    numArcs, 
-    padding.arc, 
-    startAngle, 
-    cornerRadius, 
-    data, 
-    classNameGaugeBg, 
-    tooltip, 
-    drawing, 
-    labels, 
-    labelKey, 
-    dataKey, 
-    targetKey
+    id,
+    margin,
+    endAngle,
+    PI,
+    minRadius,
+    numArcs,
+    padding.arc,
+    startAngle,
+    cornerRadius,
+    data,
+    classNameGaugeBg,
+    drawing,
+    dataKey,
+    targetKey,
   ]);
 
   useEffect(() => {
     refreshChart();
     return () => {
-      selectAll('#tooltip').remove();
+      selectAll<SVGElement, unknown>('#tooltip').remove();
     };
   }, [data, refreshChart]);
+
   return (
     <svg
       id={id}
@@ -256,16 +198,20 @@ const RingGauge = ({
 
 export default RingGauge;
 
-
-function getRadialDimensions(width: number, margin: MarginProps, height: number) {
-  const innerWidth = width - (margin.left || 0) - (margin.right || 0)
-  const innerHeight = height - (margin.top || 0) - (margin.bottom || 0)
-  const  chartRadius = Math.min(innerHeight, innerWidth) / 2;
-  return {innerWidth, innerHeight, chartRadius};
+function getRadialDimensions(
+  width: number,
+  margin: MarginProps,
+  height: number
+) {
+  const innerWidth = width - (margin.left || 0) - (margin.right || 0);
+  const innerHeight = height - (margin.top || 0) - (margin.bottom || 0);
+  const chartRadius = Math.min(innerHeight, innerWidth) / 2;
+  return { innerWidth, innerHeight, chartRadius };
 }
 
-/*********************UTILITY FUNCTIONS********************* */
-function getSVGDimensions(svg: Selection<BaseType, any, HTMLElement, any>) {
+function getSVGDimensions(
+  svg: Selection<BaseType, unknown, HTMLElement, unknown>
+) {
   if (svg.empty()) {
     return { width: 0, height: 0 };
   }
@@ -276,8 +222,4 @@ function getSVGDimensions(svg: Selection<BaseType, any, HTMLElement, any>) {
   const height = parseInt(svgStyle.height, 10) || 0;
 
   return { width, height };
-}
-
-function cleanSVG(svg: Selection<BaseType, unknown, HTMLElement, any>) {
-  svg.selectAll('*').remove();
 }
