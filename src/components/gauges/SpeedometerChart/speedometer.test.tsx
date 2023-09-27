b8ea0726daa8783ec4a65ce2584ff00550acbb53
Gowthamap <gowthamap55555@gmail.com>
2023-09-27 11:41:47 +0530

    feat: test for speedometer chart

--- src/components/gauges/SpeedometerChart/speedometer.test.tsx
+++ src/components/gauges/SpeedometerChart/speedometer.test.tsx
@@ -4,9 +4,16 @@ import { render, screen } from '@testing-library/react';
 import SpeedometerChart from '.';
 
 describe('SpeedometerChart', () => {
-  it('renders speedometer component', () => {
-    render(<SpeedometerChart data={50} id='speedometer-chart'/>);
+  it('renders speedometer component for snapshot test', () => {
+    render(<SpeedometerChart data={50} id='speedometer-chart' />);
     const speedometer = screen.getByTestId('speedometer');
     expect(speedometer).toMatchSnapshot();
   });
+
+  it('renders speedometer component', () => {
+    render(<SpeedometerChart data={50} id='speedometer-chart' />);
+    const speedometer = screen.getByTestId('speedometer');
+    expect(speedometer).toBeInTheDocument();
+  });
+
 })
