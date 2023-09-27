f10f2fce58276864c3061f414e10e7ff3b865cab
John Pathadan <john.paul@vonnue.com>
2023-09-27 12:49:29 +0530

    feat: snapshot test for BarChartStacked

---
+++ src/components/linear/BarChartStacked/BarChartStacked.test.tsx
@@ -0,0 +1,26 @@
+import { describe, expect, it } from 'vitest';
+import { render, screen } from '@testing-library/react';
+
+import BarChartStacked from '.';
+import data from './sample.json';
+
+describe('BarChartStacked', () => {
+  it('renders barchartstacked component for snapshot test', () => {
+    render(
+      <BarChartStacked
+        data={data}
+        id='barChartStacked-test'
+        x={[
+          {
+            key: 'reading',
+            className: '',
+          },
+          { key: 'value', className: '' },
+        ]}
+        y={{ key: 'name', className: '' }}
+      />
+    );
+    const barChartStacked = screen.getByTestId('barChartStacked');
+    expect(barChartStacked).toMatchSnapshot();
+  });
+});
