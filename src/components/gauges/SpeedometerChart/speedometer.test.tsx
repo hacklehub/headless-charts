309aba2fbf23a03d25307a17571f30a5170b241a
Gowthamap <gowthamap55555@gmail.com>
2023-09-27 10:56:12 +0530

    refactor: remove unused import

--- src/components/gauges/SpeedometerChart/speedometer.test.tsx
+++ src/components/gauges/SpeedometerChart/speedometer.test.tsx
@@ -1,11 +1,10 @@
 import { describe, expect, it } from 'vitest';
 import { render, screen } from '@testing-library/react';
 
-import React from 'react';
 import SpeedometerChart from '.';
 
 describe('SpeedometerChart', () => {
-  it('renders DataBox component', () => {
+  it('renders speedometer component', () => {
     render(<SpeedometerChart data={50} id='speedometer-chart'/>);
     const speedometer = screen.getByTestId('speedometer');
     expect(speedometer).toMatchSnapshot();
